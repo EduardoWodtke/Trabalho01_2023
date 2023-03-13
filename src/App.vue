00e30ff05b21e871864c47624d32af1ea544769f
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2023-03-13 11:42:27 -0300

    opa_08

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
+const nome = 'Eduardo'
+const idade = 17
+function inverter(texto) {
+  return texto.split('').reverse().join('')
+}
+function inverterMaiusculo(texto) {
+  return inverter(texto).toUpperCase()
+}
+function saudacao() {
+  return `Olá, ${nome}!`
+}
+
 const contador = ref(0)
 
 function incrementarContador() {
@@ -12,12 +24,12 @@ function decrementarContador() {
 // function maiorOuMenor(){
 //   if(contador.value >= 10){
 //     return `maior/igual`
-    
+
 //   }else{
 //     return `menor`
 //   }
 // }
-function resetar(){
+function resetar() {
   contador.value = 0
 }
 
@@ -28,27 +40,68 @@ onMounted(() => {
 </script>
 
 <template>
-  <button @click="incrementarContador">+</button>
-  <button @click="decrementarContador">-</button>
-  <button @click="resetar">C</button>
-  <p>Valor do contador é: {{ contador }}</p>
-  <div id="vermelho" v-if="contador >= 10">
-    Numero maior/igual que dez
-  </div>
-  <div id="azul" v-else>
-  Numero menor que dez
+  <div class="info">
+    <h1>Exemplo 1 - Mostrando informações na tela</h1>
+    <p>Nome: {{ nome }} </p>
+    <p>Idade: {{ idade }} anos</p>
+    <hr />
+    <p>Para completar 50 anos faltam: {{ 50 - idade }} anos</p>
+    <p>O nome tem {{ nome.length }} caracteres</p>
+    <p>O nome invertido é: {{ inverter(nome) }} </p>
+    <p>O nome invertido em maisculo é: {{ inverterMaiusculo(nome) }} </p>
+    <hr />
+    <p> Exemplo de saudação usando função</p>
+    <p> {{ saudacao() }} </p>
+    <hr />
+    <button @click="incrementarContador">+</button>
+    <button @click="decrementarContador">-</button>
+    <button @click="resetar">C</button>
+    <p>Valor do contador é: {{ contador }}</p>
+    <div id="vermelho" v-if="contador > 10">
+      O numero maior que 10
+    </div>
+    <div id="azul" v-else-if="contador < 10">
+      O numero menor que 10
+    </div>
+    <div v-else>
+      O numero é 10
+    </div>
+    <hr />
+    <div v-if="contador % 2 === 0">
+      É par
+    </div>
+    <div v-else>
+      É ímpar
+    </div>
   </div>
 </template>
 
 <style scoped>
+
+
 button {
   font-weight: bold;
   cursor: pointer;
 }
-#vermelho{
+
+#vermelho {
   color: red;
 }
-#azul{
+
+#azul {
   color: blue;
+}
+
+.info {
+  background-color: rgba(35, 12, 22, 1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+}
+
+h1 {
+  color: white;
+  font-weight: bold;
+  font-size: 1.5em;
 }
 </style>
